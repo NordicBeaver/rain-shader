0ff56e77f14c79a72ce81dc05ed5ed7b3bf8b576
NordicBeaver <nordicbeaver@gmail.com>
2021-11-30 16:02:37 +0300

    Install threejs.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import './style.css';
+import * as THREE from 'three';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
-app.innerHTML = `
-  <h1>Hi!</h1>
-`;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+app.appendChild(renderer.domElement);
+
+renderer.render(scene, camera);
