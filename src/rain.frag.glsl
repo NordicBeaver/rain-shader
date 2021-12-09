6a4af945a0bac10d0fe77564db72db4c9bbc7cfd
NordicBeaver <nordicbeaver@gmail.com>
2021-12-09 13:50:33 +0300

    Random drops.

--- src/rain.frag.glsl
+++ src/rain.frag.glsl
@@ -12,16 +12,25 @@ vec2 Random22(vec2 inputValue) {
   return vec2(Random21(inputValue, 34245.43), Random21(inputValue, 76453.435));
 }
 
-vec2 Drops(vec2 uv) {
+vec2 Drops(vec2 uv, float seed) {
   float cellsResolution = 10.0;
   vec2 uvRepeated = uv * cellsResolution;
-  vec2 cellIndex = floor(uvRepeated);
-  vec2 shift = vec2(Random11(cellIndex.y, 89432.234), 0.0);
-  vec2 cellUv = fract(uvRepeated + shift);
-  return cellUv;
+  float rowIndex = floor(uvRepeated.y);
+  vec2 shift = vec2(Random11(rowIndex, seed), Random11(0.5, seed));
+  vec2 uvRepeatedShifted = uvRepeated + shift;
+  vec2 cellIndex = floor(uvRepeatedShifted);
+  vec2 cellUv = fract(uvRepeatedShifted);
+  vec2 cellCenter = vec2(0.5);
+  float distanceFromCenter = distance(cellUv, cellCenter);
+  float isDropShown = step(0.8, Random21(cellIndex, seed));
+  float drop = smoothstep(0.2, 0.0, distanceFromCenter) * isDropShown;
+  return vec2(drop);
 }
 
 void main() {
-  vec2 drops = Drops(uvInterpolator.xy);
+  vec2 drops1 = Drops(uvInterpolator.xy, 432424.43);
+  vec2 drops2 = Drops(uvInterpolator.xy, 123213.465);
+  vec2 drops3 = Drops(uvInterpolator.xy, 674654.432);
+  vec2 drops = drops1 + drops2 + drops3;
   gl_FragColor = vec4(drops, 0.0, 1.0);
 }
