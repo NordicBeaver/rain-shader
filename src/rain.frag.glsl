ef166375e9ce96a931b6538cc8f68303ee56e0a2
NordicBeaver <nordicbeaver@gmail.com>
2021-12-09 13:12:38 +0300

    Random color for each cell.

--- src/rain.frag.glsl
+++ src/rain.frag.glsl
@@ -1,10 +1,19 @@
 in vec2 uvInterpolator;
 
+float Random21(vec2 inputValue, float seed) {
+  return fract(sin(dot(inputValue, vec2(123.456, 43.12))) * seed);
+}
+
+vec2 Random22(vec2 inputValue) {
+  return vec2(Random21(inputValue, 34245.43), Random21(inputValue, 76453.435));
+}
+
 vec2 Drops(vec2 uv) {
   float cellsResolution = 10.0;
   vec2 uvRepeated = uv * cellsResolution;
   vec2 cellIndex = floor(uvRepeated);
-  return cellIndex / cellsResolution;
+  vec2 random = Random22(cellIndex);
+  return random;
 }
 
 void main() {
