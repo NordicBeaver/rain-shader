cc1c86e6a48cfea91e1e7f5c3d88774c14690c6c
NordicBeaver <nordicbeaver@gmail.com>
2021-12-12 15:16:04 +0300

    Enable antialiasing.

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 7;
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 app.appendChild(renderer.domElement);
 
